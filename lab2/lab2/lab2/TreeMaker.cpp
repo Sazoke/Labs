1bd971243406cc3eab213ce53409a6f0bc21d238
Sazoke <44672298+Sazoke@users.noreply.github.com>
2020-04-21 15:17:02 +0500

    Added TreeMaker

---
+++ lab2/lab2/lab2/TreeMaker.cpp
@@ -0,0 +1,1 @@
+#include "TreeMaker.h"
--- lab2/lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2/lab2.cpp
@@ -3,36 +3,15 @@
 
 #include <iostream>
 #include"BinTree.h"
+#include"TreeMaker.h"
+using namespace std;
 const int length = 9;
-
-BinTree* GetTree(int*, int);
-void AddToTree(BinTree*, int*, int, int);
 
 int main()
 {
 	int arr[length];
 	for (int i = 0; i < length; i++)
 		arr[i] = i;
-	auto tree = GetTree(arr, length);
+	auto tree = TreeMaker().CreateMinimalBST(begin(arr), end(arr));
 }
 
-BinTree* GetTree(int* arr, int length) {
-	auto tree = new BinTree(arr[length / 2]);
-	if (length % 2 == 1)
-		AddToTree(tree, arr, 0, (length - 1) / 2);
-	else
-		AddToTree(tree, arr, 0, length / 2);
-	AddToTree(tree, arr, length / 2 + 1, (length - 1) / 2);
-	return tree;
-}
-
-void AddToTree(BinTree* tree, int arr[], int index, int length) {
-	if (length == 0)
-		return;
-	tree->Insert(arr[index + length / 2]);
-	if (length % 2 == 1)
-		AddToTree(tree, arr, index, (length - 1) / 2);
-	else
-		AddToTree(tree, arr, index, length / 2);
-	AddToTree(tree, arr, index + length / 2 + 1, (length - 1) / 2);
-}
