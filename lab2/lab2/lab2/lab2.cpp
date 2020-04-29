295bce8c20b979e02698f6a30b25c3f73571e41d
Sazoke <44672298+Sazoke@users.noreply.github.com>
2020-04-29 15:32:20 +0500

    Update lab2.cpp

--- lab2/lab2/lab2/lab2.cpp
+++ lab2/lab2/lab2/lab2.cpp
@@ -5,8 +5,8 @@
 #include"BinTree.h"
 using namespace std;
 const int length = 9;
+BinTree* tree = nullptr;
 BinTree* CreateMinimalBST(int*, int, int);
-void AddToTree(BinTree*, int*, int, int);
 
 int main()
 {
@@ -18,23 +18,14 @@ int main()
 
 BinTree* CreateMinimalBST(int* begin, int left, int right) {
 	int length = right - left + 1;
-	auto tree = new BinTree(*(begin + (length / 2)));
+	if(tree == nullptr)
+		tree = new BinTree(*(begin + (length / 2)));
+	else
+		tree->Insert(*(begin + (right + left) / 2));
 	if (length % 2 == 1)
-		AddToTree(tree, begin, 0, ((right - left) - 1) / 2);
+		CreateMinimalBST(begin, 0, ((right - left) - 1) / 2);
 	else
-		AddToTree(tree, begin, 0, (right - left) / 2);
-	AddToTree(tree, begin, (right - left + 1) / 2 + 1, right);
+		CreateMinimalBST(begin, 0, (right - left) / 2);
+	CreateMinimalBST(begin, length / 2 + 1, right);
 	return tree;
-}
-
-void AddToTree(BinTree* tree, int* begin, int left, int right) {
-	tree->Insert(*(begin + (right + left) / 2));
-	if (left >= right)
-		return;
-	if ((right - left) % 2 == 0)
-		AddToTree(tree, begin, left, (right + left - 1) / 2);
-	else
-		AddToTree(tree, begin, left, (right + left) / 2);
-	AddToTree(tree, begin, (left + right) / 2 + 1, right);
-
 }
