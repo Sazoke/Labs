35220171ccb55325cb65c3653716fbef97fe078b
Sazoke <44672298+Sazoke@users.noreply.github.com>
2020-05-29 22:40:03 +0500

    Update MatrixBase.h

--- lab4/lab4/MatrixBase.h
+++ lab4/lab4/MatrixBase.h
@@ -3,9 +3,6 @@
 
 class MatrixBase
 {
-protected:
-	MatrixBase(unsigned int iSize);
-	unsigned int m_size;
 public:
 	unsigned int size() const;
 	virtual int element(unsigned int i, unsigned int j) const;
@@ -13,4 +10,8 @@ public:
 	void operator *=(int iMult);
 	void operator +=(MatrixBase iAdd);
 	friend std::ofstream& operator<<(std::ofstream& os, MatrixBase& iMatrix);
+protected:
+	MatrixBase(unsigned int iSize);
+private:
+	unsigned int m_size;
 };
